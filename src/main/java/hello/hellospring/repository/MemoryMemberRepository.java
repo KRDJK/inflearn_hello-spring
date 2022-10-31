4af919a1d2894b36b40b49c4f1b90a5abefbbfe8
KRDJK <kdf556@naver.com>
2022-10-31 23:34:22 +0900

    [22.10.31] MemoryMemberRepositoryTest.java 파일에서 Optinal<>, @AfterEach, assertThat() 학습 TDD란 테스트 주도 개발 (테스트 클래스 먼저 만들고 필요한 것들 만드는거)

---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,41 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+
+    private static long sequence = 0L;
+
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+            // null이 반환될 여지가 있으면 사용
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream().filter(member -> member.getName().equals(name))
+                .findAny(); // 반복문을 돌려서 하나라도 name 값이 같은게 걸리면 걔를 반환하게 하는 작업.
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
