65fe02122e83422dde01111fd3ca2e8570eb5844
KRDJK <kdf556@naver.com>
2022-11-04 16:02:17 +0900

    [22.11.04] AOP 개념 학습 (+AOP가 필요한 상황 예시) ★ 핵심 관심사항(주된 비즈니스 로직) : core concern, 공통 관심사항(곁가지 로직 ex.메서드 동작에 걸리는 시간 측정 등) : cross-cutting concern ★

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -25,11 +25,18 @@ public class MemberService {
     * 회원 가입
     * */
     public Long join(Member member) {
+        long start = System.currentTimeMillis();
 
-        // 같은 이름이 있는 중복 회원은 안되게 해야 한다.
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
+        try {
+            // 같은 이름이 있는 중복 회원은 안되게 해야 한다.
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -50,7 +57,15 @@ public class MemberService {
     * 전체 회원 조회
     * */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish-start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
     }
 
     /*
